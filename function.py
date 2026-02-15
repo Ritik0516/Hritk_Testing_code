9723ca9048a73ad5699817b708071a66b6c1949f
Ritik0516 <singhhritik1997@gmail.com>
2026-02-15 13:06:48 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,17 @@
+def add(num1,num2):
+    add=num1 + num2
+    return(add)
+
+def sub(num1,num2):
+    sub=num1 - num2
+    return(sub)
+
+def mul(num1,num2):
+    mul=num1 * num2
+    return(mul)
+
+print(add(10,20))
+
+print(sub(10,20))
+
+print(mul(10,20))
